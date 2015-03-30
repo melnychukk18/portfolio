174b611999fda0f02d66ce2c261671015d88a3a0
melnychukk <melnychukk18@gmail.com>
2015-03-30 19:23:27 +0200

    Single work page. Works page. Latest works on mai page.

--- works/views.py
+++ works/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from works.models import WorkItem,WorkType
 
 def home(request):
-
-	return render(request, 'main/index.html')
+	last_works = WorkItem.objects.all().order_by('-pub_date')[0:3]
+	return render(request, 'main/index.html',{
+		'last_works':last_works
+		})
 
 def works(request):
 	types = WorkType.objects.all()
@@ -21,11 +23,13 @@ def single_work(request, work_pk):
 	work = get_object_or_404(WorkItem,pk=work_pk)
 	prev = work.get_prev()
 	next = work.get_next()
+	releated = WorkItem.objects.all().order_by('?')[:3]	
 	context = {
 		'pagetype':'single single-portfolio',
 		'work':work,
 		'prev':prev,
-		'next':next
+		'next':next,
+		'releated':releated
 		}
 	return render(request, 'works/single_work.html',
 		context)
